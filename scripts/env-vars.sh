de58215744471dd501e2424716b74c402f4f80c5
vinomanick <vino1393@gmail.com>
2023-12-02 10:42:43 +0530

    added sh file

---
+++ scripts/env-vars.sh
@@ -0,0 +1,5 @@
+DOCKER_IMAGE='724155539613.dkr.ecr.us-east-1.amazonaws.com/fw-chat-admin:v02'
+AWS_ACCOUNT_ID="724155539613"
+AWS_REGION="us-east-1"
+AWS_ROLE_ARN="arn:aws:iam::724155539613:role/cross_account_runway"
+SPOT_INST_LABEL="freshchat-kube-update"
